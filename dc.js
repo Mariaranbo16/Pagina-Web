24268a184f3f5c0efd0f0e466f4505bc7e837e04
Mariana Arango <marangob@udistritla.edu.co>
2024-05-14 15:23:10 -0500

    Vrs.01

---
+++ dc.js
@@ -0,0 +1,18 @@
+for (i=0; i<3; i++)
+    {
+    alert("Hola!!!")
+    }
+
+function escribir()
+{
+    document.write("fhhfhfd")
+}
+
+function numeros()
+{
+for (i=1; i<11; i++)
+    {
+        document.write(i,",")
+    }
+    
+}
